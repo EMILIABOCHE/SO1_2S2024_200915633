0caf9cffb64a12fd12bedd6584b5310baa418490
EMILIABOCHE <1763670630308@ingenieria.usac.edu.gt>
2024-08-17 19:38:08 -0600

    Se agrega tarea 2, Creación de imagenes docke

---
+++ tarea2/ejecutable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+if ! command -v docker &> /dev/null
+then
+    echo "Docker no está instalado. Por favor, instala Docker primero."
+    exit 1
+fi
+ 
+generate_random_name() {
+    cat /proc/sys/kernel/random/uuid | cut -d'-' -f1
+}
+
+ 
+for i in $(seq 1 10)
+do
+    container_name=$(generate_random_name)
+    echo "Creando contenedor con nombre: $container_name"
+    docker run -d --name "$container_name" alpine
+done
+
+echo "10 contenedores han sido creados."
+
---
+++ tarea2/readme.md
@@ -0,0 +1,28 @@
+## Universidad de San Carlos
+## Facultad de Ingenieria
+## Laboratorio de Sistemas Operativos 1
+
+<br>
+<br><br>
+
+#### Rosa Emilia Boche Naz
+#### 200915633
+
+# Tarea 2
+
+<br>
+<br>
+<br>
+
+1. Se crea el archivo bash
+<br>
+2. Se ejecuta el archivo 
+![](./imagenes/imagen1.png)
+
+
+![](./imagenes/imagen2.png)
+
+![](./imagenes/imagen3.png)
+
+3. Se muestra la información de las imagenes creadas
+![](./imagenes/imagen4.png)
